5d0fb39261393bcfb95d53cc097f38905160a6cc
r-avellar <105829975+r-avellar@users.noreply.github.com>
2022-11-16 14:04:31 -0300

    Add files via upload

---
+++ GolangItensivo/cmd/consumer/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/r-avellar/gointensivo/internal/order/infra/database"
+	"github.com/r-avellar/gointensivo/internal/order/usecase"
+	"github.com/r-avellar/gointensivo/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+func main(){
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil{
+		panic(err)
+	}
+	defer db.Close()
+	repository := database.NewOrderRepository(db)
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: repository}
+
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil{
+		panic(err)
+	}
+	defer ch.Close()
+	out := make(chan amqp.Delivery, 100)
+	go rabbitmq.Consume(ch, out)
+
+	qtdWorkers := 50
+	for i := 1; i <=qtdWorkers; i++{
+		go worker(out, &uc, i)
+	}
+
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: repository}
+		total, err := getTotalUC.Execute()
+		if err != nil{
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int){
+	for msg := range deliveryMessage{
+		var inputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &inputDTO)
+		if err != nil{
+			panic(err)
+		}
+		outputDTO, err := uc.Execute(inputDTO)
+		if err != nil{
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Printf("Worker %d has processed order %s\n", workerID, outputDTO.ID)
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ GolangItensivo/go.mod
@@ -0,0 +1,16 @@
+module github.com/r-avellar/gointensivo
+
+go 1.19
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/stretchr/testify v1.8.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.5.0
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
